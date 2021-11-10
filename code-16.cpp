e708988b504626a378d4b90e3ae501677a26a195
Kinshuk Banerjee <90320839+kinshukbanerjee899@users.noreply.github.com>
2021-11-10 21:59:36 +0530

    Add files via upload

---
+++ code-16.cpp
@@ -0,0 +1,18 @@
+// Program to demonstrate Exception Handling
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter two numbers : ";
+    cin>>x>>y;
+    try
+    {
+        if(y==0) throw y;
+        else cout<<x/y;
+    }
+    catch(int k)
+    {
+        cout<<"Second Opernd cannot be Zero";
+    }
+    return 0;
+}
