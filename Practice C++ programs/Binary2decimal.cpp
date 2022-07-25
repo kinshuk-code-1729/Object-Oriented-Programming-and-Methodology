eafcc7f3fb08925ea5e1373e34ccc48c3f659f85
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2022-07-25 16:50:35 +0530

    Update Binary2decimal.cpp

--- Practice C++ programs/Binary2decimal.cpp
+++ Practice C++ programs/Binary2decimal.cpp
@@ -2,28 +2,23 @@
 //Downloaded From www.turboc8.com coded by Yogendra Singh.
 
 #include<iostream.h>
-#include<conio.h>
 #include<math.h>
-int main()
-  {
-  clrscr();
-  cout<<"                         www.turboc8.com"<<endl;
+using namespace std;
+int main(){
   int x[10],count=0,n,sum=0,j,k=0;
-  cout<<"Enter Binary Number: ";
+  cout<<"Enter a Binary Number : ";
   cin>>n;
   
-  while(n>0)
-  {
-  x[count]=n%10;
-  n=n/10;
-  count++;
+  while(n>0){
+    x[count]=n%10;
+    n=n/10;
+    count++;
   }
   for(j=0;j<count;j++)
   {
       sum=sum+x[j]*pow(2,k);
       k++;
   }
-  cout<<"Decimal Number: "<<sum;
-  getch();
+  cout<<"Equvalent Decimal Number : "<<sum;
   return 0;
   }
