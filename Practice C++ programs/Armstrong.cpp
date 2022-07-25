e21a45bf92aad74bf611592300f540be81c57c5b
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2022-07-25 16:48:11 +0530

    Update Armstrong.cpp

--- Practice C++ programs/Armstrong.cpp
+++ Practice C++ programs/Armstrong.cpp
@@ -1,35 +1,25 @@
-//write a progrom to Check Armstrong Number (4 digit)
-//Downloaded From www.turboc8.com coded by Yogendra Singh.
-
-#include <iostream.h>
-#include<conio.h>
+//Progrom to Check for 4 digit Armstrong Number.
+#include <iostream>
+using namespace std;
 #include<math.h>
-int main()
-{
-clrscr();
-cout<<"                         www.turboc8.com"<<endl;
-float n,m,s=0,b,count=0;
-cout<<"enter the number to find ";
-cin>>n;
-b=n;
-while(n){
-      n=n/10;
-      count++;
-  }
+int main(){
+      float n,m,s=0,b,count=0;
+      cout<<"enter any number : ";
+      cin>>n;
+      b=n;
+      while(n){
+            n=n/10;
+            count++;
+      }
   
-while(n>0)
-{
-m=n%10;
-s=s+pow(m,count);
-n=n/10;
-}
+      while(n>0){
+            m=n%10;
+            s=s+pow(m,count);
+            n=n/10;
+      }
 
 
-if(b==s)
-cout<<" amstrong number is"<<b;
-else
-cout<<"not amstrong";
-getch();
+      if(b==s)    cout<<" amstrong number is"<<b;
+      else        cout<<"not amstrong";
 return 0;
 }
-
