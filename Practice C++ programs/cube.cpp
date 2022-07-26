a865f169e24e15b3c74cad1df26bf1c3d11e024d
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2022-07-26 19:25:54 +0530

    Update cube.cpp

--- Practice C++ programs/cube.cpp
+++ Practice C++ programs/cube.cpp
@@ -1,16 +1,18 @@
 // Calculating cube using a function
 #include<iostream>
 using namespace std;
-float cube(float a)
-{ float n;
-n=a*a*a;
-return(n);
+
+float cube(float a){
+  float n;
+  n=a*a*a;
+  return(n);
 }
-int main()
-{ float x,y,cube(float);
-cout<<"\nEnter any number to find its cube : ";
-cin>>x;
-y=cube(x);
-cout<<"The cube of "<<x<<" is "<<y<<"\n";
-return 0;
+
+int main(){
+  float x,y,cube(float);
+  cout<<"\nEnter any number to find its cube : ";
+  cin>>x;
+  y=cube(x);
+  cout<<"The cube of "<<x<<" is "<<y<<"\n";
+  return 0;
 }
