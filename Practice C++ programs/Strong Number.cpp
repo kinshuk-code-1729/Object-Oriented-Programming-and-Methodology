aa1de17e5cfe2a2740a12c0f6845b1a49fadc814
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2022-07-26 18:58:22 +0530

    Update Strong Number.cpp

--- Practice C++ programs/Strong Number.cpp
+++ Practice C++ programs/Strong Number.cpp
@@ -1,35 +1,24 @@
-//write a progrom to Check Strong Number.
-//Downloaded From www.turboc8.com coded by Yogendra Singh.
-
-#include<iostream.h>
-#include<conio.h>
-int main()
-{
-  clrscr();
-  cout<<"                         www.turboc8.com"<<endl; 
+//Progrom to Check whether a number is a Strong Number.
+#include<iostream>
+using namespace std;
+int main(){
   int n,i,f,r,sum=0,temp;
-
-  cout<<"Enter a number: ";
+  cout<<"Enter a number : ";
   cin>>n;
- 
   temp=n;
-  while(n)
-  {
+  while(n){
       i=1,f=1;
       r=n%10;
-
-      while(i<=r)
-      {
-         f=f*i;
+      while(i<=r){
+         f = f*i;
         i++;
       }
-      sum=sum+f;
-      n=n/10;
+      sum += f;
+      n /= 10;
   }
   if(sum==temp)
       cout<<" is a strong number";
   else
       cout<<" is not a strong number";
-  getch();
   return 0;
 }
